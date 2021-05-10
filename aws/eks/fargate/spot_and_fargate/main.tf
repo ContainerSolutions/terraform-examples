202cbb0ac7541f4f76c81818027fbbbbab31e929
Lucas Severo Alves <lucassalves65@gmail.com>
2021-05-10 12:18:33 +0200

    Add eks fargate example with spot control plane

---
+++ aws/eks/fargate/spot_and_fargate/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 0.14.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "eks-spot-and-fargate-network" {
+  source = "./network"
+  name               = var.name
+  environment        = var.environment
+  cidr               = var.cidr
+  private_subnets    = var.private_subnets
+  public_subnets     = var.public_subnets
+  availability_zones = var.availability_zones
+}
+
+
+module "eks-spot-and-fargate-cluster" {
+  source          = "./fargate_eks"
+  name            = var.name
+  environment     = var.environment
+  region          = var.region
+  k8s_version     = var.k8s_version
+  vpc_id          = module.eks-spot-and-fargate-network.id
+  private_subnets = module.eks-spot-and-fargate-network.private_subnets
+  public_subnets  = module.eks-spot-and-fargate-network.public_subnets
+}

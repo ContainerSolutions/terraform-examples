ae2dbc24c296d90c02080a46743d1e8ac784c52a
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2021-05-13 12:32:45 +0200

    Corrected check_format.sh to return 1 when format violation is found

--- bin/check_format.sh
+++ bin/check_format.sh
@@ -5,10 +5,13 @@ set -o nounset
 
 cd "${0%/*}/.."
 
-if [[ $(terraform fmt -recursive -diff > fmt.out 2>&1) != '' ]]
+terraform fmt -recursive -diff > fmt.out 2>&1
+if [[ -s fmt.out ]]
 then
   echo "Terraform fmt failed"
   cat fmt.out
   rm fmt.out
   exit 1
+else
+  rm fmt.out
 fi

7ec69ebb59b8d1ec01abf3fcf6b3a3f7f0e6d42d
Lucas Severo Alves <lucassalves65@gmail.com>
2021-05-13 15:36:55 +0200

    Keep all in main.tf

--- aws/aws_eks/fargate/spot_and_fargate/variable.tf
+++ aws/aws_eks/fargate/spot_and_fargate/variable.tf
@@ -23,11 +23,6 @@ variable "cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "private_subnets" {
-  description = "a list of CIDRs for private subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
-  default     = ["10.0.0.0/20", "10.0.32.0/20", "10.0.64.0/20"]
-}
-
 variable "public_subnets" {
   description = "a list of CIDRs for public subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
   default     = ["10.0.16.0/20", "10.0.48.0/20", "10.0.80.0/20"]

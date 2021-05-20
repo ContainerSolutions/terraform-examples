3aee94f57d5005189884b74e23379e94939f91b1
ian <ian.miell@container-solutions.com>
2021-05-20 16:34:47 +0100

    Try a cleanup script

---
+++ aws/aws_vpc/count/cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+VPC_IDS=$(aws ec2 describe-vpcs | jq -r '.Vpcs[] | select(.Tags and .Tags[].Key=="cs_terraform_examples" and .Tags[].Value=="aws_vpc/count") | .VpcId')
+count=0
+for vpc_id in $VPC_IDS
+do
+  terraform import "aws_vpc.aws_vpc_count[${count}]" $vpc_id
+  ((count++))
+done
--- aws/aws_vpc/count/run.sh
+++ aws/aws_vpc/count/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-../../../bin/apply_aws.sh
-
+cd "${0%/*}" || exit 1
+if ! ../../../bin/apply_aws.sh
+then
+  ./cleanup.sh
+  ../../../bin/apply_aws.sh
+fi
+cd - || exit 1

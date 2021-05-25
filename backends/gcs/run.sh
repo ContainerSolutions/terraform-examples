37fab5a26a6011bc2c362e24777a4627efb708f4
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2021-05-25 16:05:24 +0200

    Make shellcheck happy

--- backends/gcs/run.sh
+++ backends/gcs/run.sh
@@ -14,12 +14,14 @@ log "Set up bucket..."
 cd google_storage_bucket
 ./run.sh
 
-export BUCKET_NAME="$(terraform show -no-color | grep -w name | awk '{print $NF}')"
+BUCKET_NAME="$(terraform show -no-color | grep -w name | awk '{print $NF}')"
+export BUCKET_NAME
 log "Bucket name is: ${BUCKET_NAME}"
 cd -
 
 cd google_compute_network
 log "Setting up Terraform code from main_template in google_compute_network..."
+# shellcheck disable=SC2016
 envsubst '$BUCKET_NAME' < "main_template" > "main.tf"
 
 log "Set up vpc, storing state in backend..."

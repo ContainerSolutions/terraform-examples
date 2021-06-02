695ef2f90c8b6138b56b5c9c484713115d75776b
ian <ian.miell@container-solutions.com>
2021-06-02 16:08:24 +0100

    Bugfix: Use correct names

--- aws/aws_eks/fargate/spot_and_fargate/main.tf
+++ aws/aws_eks/fargate/spot_and_fargate/main.tf
@@ -175,12 +175,12 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "changeme_spot_and_fargate_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/changeme_spot_and_fargate_AmazonEKSClusterPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.changeme_spot_and_fargate_iam_role_cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "changeme_spot_and_fargate_AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/changeme_spot_and_fargate_AmazonEKSServicePolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.changeme_spot_and_fargate_iam_role_cluster.name
 }
 
@@ -241,17 +241,17 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "changeme_spot_and_fargate_AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/changeme_spot_and_fargate_AmazonEKSWorkerNodePolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.changeme_spot_and_fargate_iam_role_node_group.name
 }
 
 resource "aws_iam_role_policy_attachment" "changeme_spot_and_fargate_AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/changeme_spot_and_fargate_AmazonEKS_CNI_Policy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.changeme_spot_and_fargate_iam_role_node_group.name
 }
 
 resource "aws_iam_role_policy_attachment" "changeme_spot_and_fargate_AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/changeme_spot_and_fargate_AmazonEC2ContainerRegistryReadOnly"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.changeme_spot_and_fargate_iam_role_node_group.name
 }
 

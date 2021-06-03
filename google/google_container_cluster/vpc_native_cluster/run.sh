dcd6fba0da0878522d04ab0a29c10ede2034355e
ian <ian.miell@container-solutions.com>
2021-06-03 14:25:56 +0100

    No longer use import, has been replaced with central destroy call after every run

--- google/google_container_cluster/vpc_native_cluster/run.sh
+++ google/google_container_cluster/vpc_native_cluster/run.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-cd "${0%/*}" || exit 1
-# If this is non-interactive, then cleanup on failure...
-if [[ $- != *i* ]]
-then
-  ../../../bin/apply_aws.sh || ( ./import.sh && ./destroy.sh )
-  ../../../bin/apply_aws.sh
-else
-  ../../../bin/apply_aws.sh
-fi
-cd - || exit 1
+../../../bin/apply_google.sh

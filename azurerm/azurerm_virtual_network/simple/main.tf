4583609736ec0f4f3783154bde3d1abe0a8b0e4b
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2021-06-04 17:07:16 +0200

    Add bin scripts for Azure, and improvements in azurerm/azurerm_virtual_network/simple

--- azurerm/azurerm_virtual_network/simple/main.tf
+++ azurerm/azurerm_virtual_network/simple/main.tf
@@ -1,5 +1,6 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
+# Summary: A simple Azure Virtual Network
+
+# Documentation: https://www.terraform.io/docs/language/settings/index.html
 terraform {
   required_version = ">= 0.14.0"
   required_providers {
@@ -10,21 +11,31 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
+# Documentation: https://www.terraform.io/docs/language/values/variables.html
+variable "azure_subscription_id" {
+  type = string
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+# Documentation: https://www.terraform.io/docs/language/providers/requirements.html
+# Documentation: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.azure_subscription_id
+}
+
+# Resource Group
+# Documentation: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+resource "azurerm_resource_group" "changeme_simple_resource_group" {
+  name     = "changeme-simple-resource-group"
   location = "West Europe"
 }
 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+# Virtual Network within the Resource Group
+# Documentation: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
+resource "azurerm_virtual_network" "changeme_simple_virtual_network" {
+  name                = "changeme-simple-virtual-network"
+  resource_group_name = azurerm_resource_group.changeme_simple_resource_group.name
+  location            = azurerm_resource_group.changeme_simple_resource_group.location
   address_space       = ["10.0.0.0/16"]
 }

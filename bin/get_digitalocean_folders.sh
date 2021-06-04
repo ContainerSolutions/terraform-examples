579f4b1e493450529289c63687bf6fa2ed187263
Roman Zhuzha <romazhuzha@gmail.com>
2021-06-04 13:42:45 +0200

    fix digitalocean folders search
    and a some docs

--- bin/get_digitalocean_folders.sh
+++ bin/get_digitalocean_folders.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # shellcheck disable=SC2034
-LINODE_FOLDERS="$(find digitalocean | grep tf$ | xargs -n1 dirname | sed 's/^.\///')"
+DIGITALOCEAN_FOLDERS="$(find digitalocean | grep tf$ | xargs -n1 dirname | sed 's/^.\///')"

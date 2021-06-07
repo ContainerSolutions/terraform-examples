c6bd1ba65ecb6c4e0fd3e55ab302c9724d8998a1
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2021-06-07 10:25:10 +0200

    Ignore variable 'azure_subscription_id' in bin/check_non_unique_variable.sh

--- bin/check_non_unique_variable.sh
+++ bin/check_non_unique_variable.sh
@@ -7,7 +7,7 @@ cd "${0%/*}/.."
 
 echo "Running $0 ..."
 
-IGNORE_VARIABLE_NAMES='project_id' # variable names list separated with '|'
+IGNORE_VARIABLE_NAMES='project_id|azure_subscription_id' # variable names list separated with '|'
 VARIABLE_NAMES="$(find . -print0 -name '*\.tf' | xargs -0 grep -rnwI ^variable | awk "!/${IGNORE_VARIABLE_NAMES}"'/{print $2}' | sort -u | sed 's/"\(.*\)"/\1/')"
 FAILED=0
 

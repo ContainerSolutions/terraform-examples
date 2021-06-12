c0643e764a222ceee09c8e08ffa94beecb3103d6
ian <ian.miell@container-solutions.com>
2021-06-12 07:00:12 +0100

    Only validate changes

--- bin/check_validate.sh
+++ bin/check_validate.sh
@@ -13,6 +13,8 @@ cd "${0%/*}/.."
 echo "================================================================================"
 echo "Validating every folder's terraform code"
 echo "================================================================================"
+changed_folders=$(./bin/get_changed_folders.sh)
+last_successful_commit=$(./bin/get_last_successful_commit.sh)
 TERRAFORM_FOLDERS="$(find . | grep tf$ | xargs -n1 dirname | sed 's/^.\///')"
 for folder in $TERRAFORM_FOLDERS
 do
@@ -22,6 +24,11 @@ do
   if [ "${folder}" = "backends/remote" ] && [ ! -f "${HOME}/.terraform.d/credentials.tfrc.json" ]
   then
     echo "Skipping validate in ${folder} as it requires 'terraform login'"
+    continue
+  fi
+  if ! echo "$changed_folders" | tr ' ' '\n' | grep "$folder"
+  then
+    echo "Folder ${folder} has not changed since last successful test on main (${last_successful_commit})"
     continue
   fi
   cd "${folder}" >/dev/null || exit 1

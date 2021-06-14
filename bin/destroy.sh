41df2cd0429b80903231b5db32876463468d8f1e
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2021-06-14 13:35:28 +0200

    Generic bin/apply.sh and bin/destroy.sh scripts

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+SUPPORTED_PROVIDERS="aws|azurerm|digitalocean|google|kubernetes|linode"
+if [[ -n "$1" && ! "$1" =~ ${SUPPORTED_PROVIDERS} ]]
+then
+  echo "usage: $0 [PROVIDER]"
+  echo "  PROVIDER := {${SUPPORTED_PROVIDERS}}"
+  exit 1
+fi
+
 cd "${0%/*}" || exit 1
 
-# shellcheck disable=SC1091
-source ./shared.sh
+if [ -z "$1" ]
+then
+  # shellcheck disable=SC1091
+  source ./shared.sh
+else
+  # shellcheck disable=SC1091
+  # shellcheck source=shared_aws.sh
+  source "./shared_${1}.sh"
+fi
 # shellcheck disable=SC1091
 source ./shared_terraform_cloud.sh
 

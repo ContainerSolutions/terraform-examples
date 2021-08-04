e8756893250fb8c270a6cde78951c341f9319278
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2021-08-04 09:57:10 +0200

    [aws_dynamodb_table] Fix format to make 'terraform fmt' happy 	modified:   aws/aws_dynamodb/table/main.tf

--- aws/aws_dynamodb/table/main.tf
+++ aws/aws_dynamodb/table/main.tf
@@ -30,17 +30,13 @@ variable "changeme_hash_key" {
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table#argument-reference
 resource "aws_dynamodb_table" "changeme_aws_dynamodb_table" {
-  # Explanation: the table's name, which needs to be unique to a region
-  name           = "changeme_table_name"
-  # Explanation: the number of read units for the table
-  read_capacity  = 10
-  # Explanation: the number of write units for the table
-  write_capacity = 10
+  name           = "changeme_table_name" # Explanation: the table's name, which needs to be unique to a region
+  read_capacity  = 10                    # Explanation: the number of read units for the table
+  write_capacity = 10                    # Explanation: the number of write units for the table
   hash_key       = var.changeme_hash_key
 
   attribute {
-    name = var.changeme_hash_key
-    # Explanation: the attribute must be of type [S]tring, [N]umber, or [B]inary
+    name = var.changeme_hash_key # Explanation: the attribute must be of type [S]tring, [N]umber, or [B]inary
     type = "S"
   }
 }

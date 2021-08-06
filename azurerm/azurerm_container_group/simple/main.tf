f24ae495ee57f3467d9a1ecc339f6913e0ac6e38
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2021-08-06 16:55:25 +0200

    Defined container ports in azurerm/azurerm_container_group/simple

--- azurerm/azurerm_container_group/simple/main.tf
+++ azurerm/azurerm_container_group/simple/main.tf
@@ -45,5 +45,10 @@ resource "azurerm_container_group" "changeme_simple_container_group" {
     image  = "microsoft/aci-helloworld:latest"
     cpu    = "0.5"
     memory = "1.5"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
   }
 }

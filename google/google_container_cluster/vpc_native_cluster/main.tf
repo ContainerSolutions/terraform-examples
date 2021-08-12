e4c94108acaef26e011c68970199b5bd54d3886e
Tomasz Tarczynski <ttarczynski@users.noreply.github.com>
2021-08-12 13:39:05 +0200

    Update terraform to v1 (#85)
    * [Update_terraform_to_v1] Set 'required_version = ">= 1.0.0"'
    in all examples
    
    * [Update_terraform_to_v1] Define Terraform and TFLint version explicitly
    in .github/workflows/main.yml
    
    * [Update_terraform_to_v1] Add terraform_wrapper: false
    to Setup Terraform
    
    * Upgrade to terraform v1.0.0 (remaining examples)
    
    * Change cidr_block in aws/aws_vpc/simple to avoid conflicts...
    with other examples
    
    * Increase timeouts for create aws_route (from 2 to 5 minutes)
    
    * Fix race condition causing 'terraform apply' errors.
    in: aws/aws_iam/groups

--- google/google_container_cluster/vpc_native_cluster/main.tf
+++ google/google_container_cluster/vpc_native_cluster/main.tf
@@ -3,7 +3,7 @@
 
 # Documentation: https://www.terraform.io/docs/language/settings/index.html
 terraform {
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.0.0"
   required_providers {
     google = {
       source  = "hashicorp/google"

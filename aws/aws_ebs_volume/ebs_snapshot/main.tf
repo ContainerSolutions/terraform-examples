abf94b8230f3d283de515d0a4225dcd7d7068072
Rodrigo Rios <rodrigo.rras@gmail.com>
2021-08-26 05:24:01 -0300

    fix attepmt #2

--- aws/aws_ebs_volume/ebs_snapshot/main.tf
+++ aws/aws_ebs_volume/ebs_snapshot/main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 }
 
 # Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/availability_zones
-data "aws_availability_zones" "changeme_az_list" {
+data "aws_availability_zones" "changeme_az_list_ebs_snapshot" {
   state = "available"
 }
 
@@ -31,7 +31,7 @@ data "aws_availability_zones" "changeme_az_list" {
 # Explanation: The AWS 'aws_ebs_volume' resource is responsible for creating the EBS volume. The only required field is Availability Zone
 # where the EBS vol is going to be created. Some optional arguments are Size in GBi, encryption(TRUE/FALSE) and he type of EBS volume. Can be "standard", "gp2", "io1", "sc1" or "st1" (Default: "standard").
 resource "aws_ebs_volume" "changeme_ebs_volume_snapshot" {
-  availability_zone = data.aws_availability_zones.changeme_az_list.names[0]
+  availability_zone = data.aws_availability_zones.changeme_az_list_ebs_snapshot.names[0]
   size              = 10
   type              = "standard"
   encrypted         = false

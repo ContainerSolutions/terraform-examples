a5321e2aae24edcd855d3ac6ffe40a758cc4119c
Tibor Eszes <teszes@pm.me>
2021-09-27 13:31:22 +0200

    Working Group - WIP

--- google/google_cloud_functions/simple/main.tf
+++ google/google_cloud_functions/simple/main.tf
@@ -1,16 +1,34 @@
+# Summary: A simple Cloud Function example with a dummy Python function
+
 # Documentation: https://www.terraform.io/docs/language/settings/index.html
-
-// tbd: add terraform settings here
-
-# Documentation: https://www.terraform.io/docs/language/providers/requirements.html
-
-// tbd: add gcp provider here
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.0"
+    }
+  }
+}
 
 # Documentation: https://www.terraform.io/docs/language/values/variables.html
+variable "project_id" {
+  type = string
+}
 
-// tbd: decalre inputs here
+# Documentation: https://www.terraform.io/docs/language/providers/requirements.html
+provider "google" {
+  project = var.project_id
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
 
-// tbd: zip up our source code
+# Documentation: https://registry.terraform.io/providers/hashicorp/archive/latest/docs/data-sources/archive_file
+data "archive_file" "init" {
+  type        = "zip"
+  source_file = "${path.module}/init.tpl"
+  output_path = "${path.module}/files/init.zip"
+}
 
 # Documentation: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
 

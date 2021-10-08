25f39973b92ec7602046cc434a933856ef743ed9
Mo Abdalla <mohamed.abdalla@container-solutions.com>
2021-10-08 15:54:13 +0100

    WIP: added alias record template

---
+++ aws/aws_route53/alias_record/main.tf
@@ -0,0 +1,15 @@
+# Summary: Creates a simple route53 alias record and a static S3 site
+
+# Documentation: https://www.terraform.io/docs/language/settings/index.html
+
+# Documentation: https://www.terraform.io/docs/language/providers/requirements.html
+
+# Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
+
+# Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+
+# Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object
+
+# Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_zone
+
+# Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record

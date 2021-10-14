fa722a34d822b5d4f8fb684877e9f4d97051c60e
Mo Abdalla <mohamed.abdalla@container-solutions.com>
2021-10-14 15:01:50 +0100

    added template for simple iam user

---
+++ aws/aws_iam/simple/main.tf
@@ -0,0 +1,11 @@
+# Summary: Create a simple IAM user
+
+# Documentation: https://www.terraform.io/docs/language/settings/index.html
+
+# Documentation: https://www.terraform.io/docs/language/providers/requirements.html
+
+# Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy
+
+# Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_user
+
+# Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_access_key

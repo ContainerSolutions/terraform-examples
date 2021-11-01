6ac77d8b7966a7d9712781ef6f0338779bdf29f2
Tibor Eszes <teszes@pm.me>
2021-11-01 10:21:09 +0100

    Add API Management Logger

---
+++ azurerm/azurerm_api_management_logger/simple/main.tf
@@ -0,0 +1,54 @@
+# Summary: A simple Azure Load Balancer
+
+# Documentation: https://www.terraform.io/docs/language/settings/index.html
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Documentation: https://www.terraform.io/docs/language/values/variables.html
+variable "azure_subscription_id" {
+  type = string
+}
+
+# Documentation: https://www.terraform.io/docs/language/providers/requirements.html
+# Documentation: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.azure_subscription_id
+}
+
+# Resource Group
+# Documentation: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+resource "azurerm_resource_group" "changeme_simple_api_management_logger_resource_group" {
+  name     = "changeme-simple-api-management-logger-resource-group-name"
+  location = "West Europe"
+}
+
+# API Management Service
+# Documentation: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/api_management
+resource "azurerm_api_management" "changeme_simple_api_management_logger_api_management" {
+  name                = "changeme-simple-api-management-logger-api-management-name"
+  location            = "West Europe"
+  resource_group_name = azurerm_resource_group.changeme_simple_api_management_logger_resource_group.name
+
+  publisher_name      = "changeme-publisher"
+  publisher_email     = "changeme-publisher@example.com"
+
+  sku_name = "Developer_1"
+}
+
+# Logger within an API Management Service
+# Documentation: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/api_management_logger
+resource "azurerm_api_management_logger" "changeme_simple_api_management_logger" {
+  api_management_name = azurerm_api_management.changeme_simple_api_management_logger_api_management.name
+  resource_group_name = azurerm_resource_group.changeme_simple_api_management_logger_resource_group.name
+
+  name = "changeme-simple-api-management-logger-name"
+}

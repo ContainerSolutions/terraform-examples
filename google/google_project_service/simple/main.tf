dd617ae7610a55df38a65fe28c670692a9254a94
Melih Cinalioğlu <mcinalioglu@hotmail.com>
2022-01-26 18:16:44 +0300

    prevent destroy on disable

--- google/google_project_service/simple/main.tf
+++ google/google_project_service/simple/main.tf
@@ -33,5 +33,6 @@ resource "google_project_service" "changeme_project_service" {
     update = "40m"
   }
 
-  disable_dependent_services = true
+  disable_dependent_services = false
+  disable_on_destroy = false
 }

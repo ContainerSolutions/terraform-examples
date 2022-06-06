2ed7fc5ee97d5bebb267aab76356332b88d08997
Melih Cinalioglu <melih.cinalioglu@container-solutions.com>
2022-06-06 18:03:20 +0300

    fixing shellcheck issues

--- bin/create_index.sh
+++ bin/create_index.sh
@@ -10,13 +10,13 @@ set -o nounset
 PROVIDER_LIST=$(find . -maxdepth 1 -mindepth 1 -type d -not -path './\.*' -not -path './bin' | sed 's/\.\///g' | sort)
 
 for provider in $PROVIDER_LIST; do
-    echo $provider
-    examples=$(find $provider -name main.tf)
+    echo "$provider"
+    examples=$(find "$provider" -name main.tf)
     for example in $examples; do
-        echo $example
-        resource=$(echo $example | cut -d '/' -f 2)
-        feature=$(echo $example | cut -d '/' -f 3)
-        summary=$(head -n1 $example | sed 's/^# Summary: //g')
+        echo "$example"
+        resource=$(echo "$example" | cut -d '/' -f 2)
+        feature=$(echo "$example" | cut -d '/' -f 3)
+        summary=$(head -n1 "$example" | sed 's/^# Summary: //g')
         echo "resource: $resource, feature: $feature, summary: $summary"
     done
     echo
